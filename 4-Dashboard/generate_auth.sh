5ad29f66fb161b17ad46d6d2cf4d937b4532ba0e
Gad Krumholz <gad.krumholz@gmail.com>
2022-02-05 21:44:18 -0600

    Added wordpress and storageprovider with rwx and updated to latest Helm

--- 4-Dashboard/generate_auth.sh
+++ 4-Dashboard/generate_auth.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-if [[ $# -eq 0 ]] ; then
-    echo "Run the script with the required auth user and namespace for the secret: ${0} [user] [namespace]"
-    exit 0
-fi
-printf "${1}:`openssl passwd -apr1`\n" >> ingress_auth.tmp
-kubectl delete secret -n ${2} ingress-auth
-kubectl create secret generic ingress-auth --from-file=ingress_auth.tmp -n ${2}
-rm ingress_auth.tmp
+token=$(kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}")
+echo $token
